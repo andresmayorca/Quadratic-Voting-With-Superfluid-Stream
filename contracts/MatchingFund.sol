8277dff2693498b2aa485594358a082f523691cd
ethcode1 <mayorcaalejandro21@gmail.com>
2022-07-14 17:37:23 -0400

    add dai.sol and update MatchingFund.sol

--- contracts/MatchingFund.sol
+++ contracts/MatchingFund.sol
@@ -8,27 +8,28 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MatchingFund{
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    struct request {
-        string description;
-        uint value;
-        address recipient;
-        bool complete;
-    }
-
+contract MatchingFund {
+    
     address public owner;
-    uint public MinimumContribution;
-    address[] public approvals;
+    IERC20 public dai;
 
-    function compaing(uint minimum) public {
-        owner = msg.sender;
-        MinimumContribution = minimum;
-    }                         
+    event PaymentDone (
+        address payer,
+        uint amount,
+        uint paymentId,
+        uint date
+    );
 
-    function contribute() public payable{
-        require(msg.value > MinimumContribution);
-        approvals.push(msg.sender);
-        console.log("funds sent successfully!");
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-} 
+
+   constructor(address ownerAddress, address daiAddress) {
+    owner = ownerAddress;
+    dai = IERC20(daiAddress);
+   } 
+
+    function pay(uint amount, uint paymentId) external {
+        dai.transferFrom(msg.sender, owner, amount);
+        emit PaymentDone(msg.sender, amount, paymentId, block.timestamp);
+    } 
+}        
